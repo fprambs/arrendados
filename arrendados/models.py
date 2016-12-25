89ff8d55d86105d19579742298d62f1c58f8f073
Felipe Prambs <felipe._16@live.com>
2016-12-25 01:42:23 -0300

    Mi primer commit

---
+++ arrendados/models.py
@@ -0,0 +1,73 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Tipo_Usuario(models.Model):
+    tipo = models.CharField(max_length=45)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=80)
+    fecha_nacimiento = models.DateField()
+    email= models.CharField(max_length=80, unique=True)
+    password = models.CharField(max_length=15)
+    telefono = models.IntegerField(null= True)
+    direccion = models.CharField(max_length=80,null=True)
+    check_offer = models.NullBooleanField(default=False)
+    tipo_usuario = models.ForeignKey(Tipo_Usuario)
+
+class Region(models.Model):
+    region = models.CharField(max_length=30)
+
+class Comuna(models.Model):
+    comuna = models.CharField(max_length=30)
+    region = models.ForeignKey(Region)
+
+class Ciudad(models.Model):
+    ciudad = models.CharField(max_length=30)
+    comuna = models.ForeignKey(Comuna)
+
+
+class Tipo_Propiedad(models.Model):
+    tipo = models.CharField(max_length=30)
+
+class Propiedad(models.Model):
+    direccion = models.CharField(max_length=80)
+    cantidad_disponible = models.IntegerField()
+    cantidad = models.IntegerField()
+    latitud = models.CharField(max_length=40)
+    longitud = models.CharField(max_length=40)
+    ciudad = models.ForeignKey(Ciudad)
+    tipo_propiedad = models.ForeignKey(Tipo_Propiedad)
+
+
+class Foto(models.Model):
+    ruta = models.CharField(max_length=200)
+    propiedad= models.ForeignKey(Propiedad)
+
+#Arrendatario
+class Usuario_Ocupa_Propiedad(models.Model):
+     usuario = models.ForeignKey(Usuario)
+     propiedad = models.ForeignKey(Propiedad)
+     fecha_inicio = models.DateTimeField(primary_key=True)
+     fecha_termino = models.DateTimeField(null=True)
+#Arrendador   
+class Usuario_Arrienda_Propiedad(models.Model):
+     usuario = models.ForeignKey(Usuario)
+     propiedad = models.ForeignKey(Propiedad)
+     fecha_inicio = models.DateTimeField(primary_key=True)
+     fecha_termino = models.DateTimeField(null=True)
+
+
+class Usuario_Califica_Usuario(models.Model):
+    usuario1 = models.ForeignKey(Usuario,related_name='usuario1')
+    usuario2 = models.ForeignKey(Usuario, related_name='usuario2')
+    fecha = models.DateField()
+    calificacion = models.IntegerField()
+
+class Usuario_Califica_Propiedad(models.Model):
+    fecha = models.DateField()
+    calificacion = models.IntegerField()
+    comentario = models.CharField(max_length=500,null=True)
+    usuario = models.ForeignKey(Usuario)
+    propiedad = models.ForeignKey(Propiedad)
---
+++ arrendados/views.py
@@ -0,0 +1,81 @@
+from arrendados.models import Usuario, Tipo_Usuario
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import StreamingHttpResponse
+from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
+
+
+@csrf_exempt
+def Users(request):
+    if request.method == 'GET':
+        usuarios = Usuario.objects.all()
+        usuarios = serializers.serialize("json",usuarios)
+        return HttpResponse(usuarios,content_type='application/json')
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            nombre = data['nombre']
+            fecha_nacimiento = data['fecha_nacimiento']
+            email = data['email']
+            password = data['password']
+            telefono = data['telefono']
+            direccion = data['direccion']
+            tipo_usuario = data['tipo_usuario']
+            check_offer = data['check_offer']
+            tipo_usuario = data['tipo_usuario']
+
+            objeto_tipo_usuario = Tipo_Usuario.objects.get(id=tipo_usuario)
+
+            usuario = Usuario(nombre=nombre,fecha_nacimiento=fecha_nacimiento,email=email,password=password,telefono=telefono,direccion=direccion, check_offer=check_offer,tipo_usuario=objeto_tipo_usuario)
+            usuario.save() 
+
+            if usuario.id is None:
+                unsuccessful={'Response':'Not Created'}
+                successful_json = json.dumps(unsuccesful)
+                return HttpResponse(unsuccessful, content_type='application/json', status=400)
+
+            else:
+                successful ={'Response':'Created','id':usuario.id}
+                successful_json = json.dumps(successful)
+                return HttpResponse(successful_json, content_type='application/json', status=201)
+        except IntegrityError as c:
+            error ={'Response':'Duplicate E-mail'}
+            error_json = json.dumps(error)
+
+            return HttpResponse(error_json, content_type='application/json', status=400)
+
+@csrf_exempt
+def User(request,id="0"):
+    if request.method == 'GET':
+        usuario = Usuario.objects.filter(id=id)
+        if usuario.exists() == True:
+            usuario = serializers.serialize("json",usuario)
+            return HttpResponse(usuario, content_type='application/json')
+        else:
+            print("Hola")
+            error ={'Response':'El usuario no existe'}
+            error_json = json.dumps(error)
+            return HttpResponse(error_json, content_type='application/json', status=200)
+
+
+    if request.method == 'DELETE':
+        try:
+            usuario = Usuario.objects.get(id=id)
+            usuario.delete()
+
+            
+            if usuario.id is None:
+                response ={'response': 'El usuario de id: ' + str(id) + ' ha sido eliminado correctamente', 'code': '200', 'status': 'OK'}
+                successful_json = json.dumps(response)
+                return HttpResponse(successful_json, content_type='application/json', status=200)
+
+        except ObjectDoesNotExist as c:
+                response ={'response': 'El usuario de id: ' + str(id) + ' no existe', 'code': '500', 'status': 'ERROR'}
+                successful_json = json.dumps(response)
+                return HttpResponse(successful_json, content_type='application/json', status=500)
+    
+
